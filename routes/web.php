05f9b71bba116b12f18fa314a177414fea63fddb
malek529 <2015710206067@ogrenci.karabuk.edu.tr>
2020-12-06 22:05:13 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
