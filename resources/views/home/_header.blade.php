676348833c01b3bceaf4eb461a1e93d79a3d0c81
Malek almubarak <73136397+Malek-Almubarak@users.noreply.github.com>
2021-02-03 18:46:46 +0300

    Setting has been made

--- resources/views/home/_header.blade.php
+++ resources/views/home/_header.blade.php
@@ -9,26 +9,39 @@
                     <div id="navigation">
                         <ul>
                             <li class="active"><a href="index.html" title="Home">Home</a></li>
-                            <li class="has-sub"><a href="service-list.html" title="Service List">Service List</a>
+
+                            @php
+                                $parentCategories=\App\Http\Controllers\HomeController::categoryList();
+                            @endphp
+                            <li class="has-sub"><a href="service-list.html" title="Service List">Services</a>
                                 <ul>
-                                    <li><a href="service-list.html" title="Service List">Service List</a></li>
-                                    <li><a href="service-detail.html" title="Service Detail">Service Detail</a></li>
+                                    @foreach($parentCategories as $rs)
+                                    <li><a href="service-list.html" title="Service List">{{$rs->title}}</a></li>
+                                    @endforeach
                                 </ul>
-                            </li>
-                            <li class="has-sub"><a href="blog-default.html" title="Blog ">News</a>
-                                <ul>
-                                    <li><a href="blog-default.html" title="Blog">Blog Default</a></li>
-                                    <li><a href="blog-single.html" title="Blog Single ">Blog Single</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#" title="Features">Features</a>
-                                <ul>
-                                    <li><a href="testimonial.html" title="Service List">Testimonial</a></li>
-                                    <li><a href="appointment.html" title="appointment">appointment</a></li>
-                                </ul>
+
                             </li>
                             <li><a href="contact.html" title="Contact Us">Contact</a> </li>
-                            <li><a href="appointment.html" title="appointment">appointment</a> </li>
+                            <li><a href="appointment.html" title="appointment">About Us</a> </li>
+                            <li><a href="appointment.html" title="appointment">References</a> </li>
+                            @auth
+                            <li class="has-sub"><a href="blog-default.html" title="Blog ">{{Auth::user()->name}}</a>
+                                <ul>
+                                    <li><a href="blog-default.html" title="Blog">My Profile</a></li>
+                                    <li><a href="blog-single.html" title="Blog Single ">Logout</a></li>
+                                </ul>
+                            </li>
+                            @else
+                                <li class="has-sub"><a href="blog-default.html" title="Blog ">JOIN/LOGIN</a>
+                                    <ul>
+                                        <li><a href="/login" title="Blog">Login</a></li>
+                                        <li><a href="/register" title="Blog Single ">Register</a></li>
+                                    </ul>
+                                </li>
+
+                            @endauth
+
+
                         </ul>
                     </div>
                 </div>
